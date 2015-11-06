3d91ce19e18acdc6b444a89cd7b2358a40fe4b49
andrei <thulf.m@gmail.com>
2015-11-07 09:01:55 +1000

    basic player without sorting

--- Hubl.Core/Service/IMusicPlayer.cs
+++ Hubl.Core/Service/IMusicPlayer.cs
@@ -1,7 +1,20 @@
-﻿namespace Hubl.Core.Service
+﻿using Hubl.Core.Model;
+using System.Collections.Generic;
+
+namespace Hubl.Core.Service
 {
 	public interface IMusicPlayer
     {
+		// IMusicPlayer (IMusicPlayerBackend backend);
 
+		PlaylistEntry QueueTrack (User user, Track track);
+		PlaylistEntry CurrentPlayedEntry { get;  } // rules of sorting by priority are not applied to this track, i think it should be given separately from the playlist
+		Queue<PlaylistEntry> Playlist { get;  } //TODO: queue??
+		Track GetTrackInfo (string path);
+
+		void Play ();
+		void Stop ();
+		//TODO: what a shame, player can't pause!
+		// void Pause ();
     }
 }
