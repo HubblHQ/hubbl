67ed4f47fc7368a579892d40b17afef8354447de
Кирилл Журавлёв <olinyavod@live.com>
2015-11-08 11:14:55 +1000

    Исправил ошибки опиляции

--- ToyConsolePlayer/Program.cs
+++ ToyConsolePlayer/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Threading;
-using Autofac;
-using Hubl.Core.Messages;
-using Hubl.Core.Service;
-using Hubl.Daemon.Message;
-using Hubl.Daemon.Network;
-using Hubl.Daemon.Service;
 
 namespace ToyConsolePlayer
 {
