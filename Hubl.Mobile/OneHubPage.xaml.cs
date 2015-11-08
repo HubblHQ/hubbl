28dc6060bae4365f4bf4647e90367a2ced5f1d9c
mesenev <heredemonsnearby@mail.ru>
2015-11-08 13:58:38 +1000

    * Info.plist: * AppDelegate.cs: * iOSPlayerBackend.cs: * Hubl.Mobile.iOS.csproj: * testsound.mp3: * OneHubPage.xaml.cs: * VkApiService.cs:
    * OneHubPage.xaml: working with mobile player

--- Hubl.Mobile/OneHubPage.xaml.cs
+++ Hubl.Mobile/OneHubPage.xaml.cs
@@ -5,6 +5,9 @@ using Autofac;
 using Xamarin.Forms;
 using Hubl.Core.Model;
 using Hubl.Core.Service;
+using Autofac.Core.Lifetime;
+using System.Linq;
+using System.Diagnostics;
 
 namespace Hubl.Mobile
 {	
@@ -25,23 +28,45 @@ namespace Hubl.Mobile
 
 	public partial class OneHubPage : ContentPage
 	{
-		ObservableCollection<Song> songs = new ObservableCollection<Song>();
 		User hub;
+		bool isPlaying;
+		ObservableCollection<Track> tracks;
+		Track currentTrack;
 		public OneHubPage (User hub)
 		{
 			this.hub = hub;
 			InitializeComponent ();
 			BindingContext = this;
-			//songs.Add (new Song{ Name = "Lose yourself", Author = "Eminem" });
-			//songs.Add (new Song{ Name = "Remember the name", Author = "Fort Minor" });
-			SongsView.ItemsSource = App.Container.Resolve<ISession> ().Playlist;
-			CurrentSong.Text = "Current song";
-			ElapsedTime.Text = TimeSpan.FromSeconds (100).ToString (@"mm\:ss");
-			RemainingTime.Text = TimeSpan.FromSeconds (150).ToString (@"mm\:ss");
-			SongProgress.ProgressTo (100.0/250, 5, Easing.CubicOut);
+			Play.Clicked += Play_Clicked;
+			tracks = App.Container.Resolve<ISession> ().Playlist;
+			SongsView.ItemsSource = tracks;
+			tracks.CollectionChanged += Tracks_CollectionChanged;
+
+			//CurrentSong.Text = "Current song";
+			//ElapsedTime.Text = TimeSpan.FromSeconds (100).ToString (@"mm\:ss");
+			//RemainingTime.Text = TimeSpan.FromSeconds (150).ToString (@"mm\:ss");
+			//SongProgress.ProgressTo (100.0/250, 5, Easing.CubicOut);
 			ToolbarItems.Add (new ToolbarItem("Add", "", async () => {
 				Navigation.PushAsync (new SongsPage(hub));	
 			}));
+		}
+
+		void Tracks_CollectionChanged (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			if (tracks.Count == 1) {
+				currentTrack = tracks.First ();
+				DependencyService.Get<IMusicPlayerBackend> ().PlayTrack (currentTrack);
+				var player = DependencyService.Get<IMusicPlayerBackend> ();
+			}
+		}
+
+		void Play_Clicked (object sender, EventArgs e)
+		{
+			if (isPlaying) {
+				DependencyService.Get<IMusicPlayerBackend> ().PauseCurrentTrack ();
+			} else {
+				DependencyService.Get<IMusicPlayerBackend> ().PlayTrack (currentTrack);
+			}
 		}
 	}
 }
---
+++ iOS/iOSPlayerBackend.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AVFoundation;
+using System.Runtime.InteropServices;
+using Hubl.Core.Service;
+using Hubl.Core.Model;
+using Foundation;
+using CoreMedia;
+using CoreText;
+using UIKit;
+using System.IO;
+using PassKit;
+using AudioToolbox;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Hubl.Mobile.iOS.iOSPlayerBackend))]
+namespace Hubl.Mobile.iOS
+{
+
+	public class iOSPlayerBackend : IMusicPlayerBackend
+	{
+		MusicPlayer player;
+		public iOSPlayerBackend()
+		{
+			
+		}
+		#region IMusicPlayerBackend implementation
+		public Track GetTrackInfo (string path)
+		{
+			throw new NotImplementedException ();
+		}
+		public void PlayTrack (Track track)
+		{
+			var path = Path.Combine (NSBundle.MainBundle.ResourcePath, "testsound.mp3");
+			var data = System.IO.File.ReadAllBytes (path);
+			var url = NSUrl.FromFilename (path);
+			var status = MusicPlayerStatus.Success;
+			player = MusicPlayer.Create (out status);
+			player.MusicSequence = new MusicSequence ();
+			player.MusicSequence.LoadFile (url, MusicSequenceFileTypeID.Any);
+			player.Start ();
+			/*AppDelegate.Player = AVAudioPlayer.FromUrl (url);
+			AppDelegate.Player.NumberOfLoops = 1;
+
+			var res = AppDelegate.Player.PrepareToPlay ();
+			//player.AddPeriodicTimeObserver (CoreMedia,CMTime.FromSeconds (1, 1), );
+
+			var r = AppDelegate.Player.Play ();
+			AppDelegate.Player.DecoderError += (sender, e) => {
+				var a = 3;
+			};
+			AppDelegate.Player.FinishedPlaying += (sender, e) => {
+				var a = 4;
+			};
+			AppDelegate.Player.BeginInterruption += (sender, e) => {
+				var a = 5;
+			};
+			AppDelegate.Player.EndInterruption += (sender, e) => {
+				var a = 6; 
+			};
+			*/
+		}
+		public void PauseCurrentTrack ()
+		{
+			var aa = 4;
+		}
+		public Track CurrentPlayedTrack {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+		#endregion
+			
+	}
+}
+
